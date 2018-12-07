8d55341339d6ef14842ea446d8dd0fadbbce92de
JJungJieun <en7615@naver.com>
2018-12-07 09:12:16 -0800

    dynamic_gh

--- web/test_graph.php
+++ web/test_graph.php
@@ -1,16 +1,56 @@
 <?php
-/*  session_start();
+//  session_start();
   require("db_config.php");
   require("function.php");
   $conn= db_init($config['host'],$config['duser'],$config['dpasswd'],$config['dname']);
   mysqli_query($conn,"set names utf8");
-*/
+
   $name='jieun';
   $telecom='kt';
 
- # $result=mysqli_query($conn,'SELECT * FROM update_data WHERE name ='.$name.' ORDER BY id DESC'); 
-  echo 'SELECT * FROM update_data WHERE name ='.$name.' ORDER BY id DESC' //데이터 조회
+  $result=mysqli_query($conn,'SELECT * FROM update_data WHERE name ="'.$name.'" ORDER BY id DESC'); 
+  var_dump($result);
+
+  while($row = mysqli_fetch_array($result)) {
+    echo '<h2>'.$row['name'].'</h2>';
+    echo $row['date'];
+  }
+
  ?>
+
+
+<html>
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>      
+    <script type="text/javascript"> 
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Day');
+      data.addColumn('number', 'price');
+      for (var i = 0; i <3; i++) {
+        data.addRow(['a',i]); //here is values to change [data,price]
+      }
+
+      var options = {
+        title: 'Test_graph',
+        width: 900,
+        height: 500,
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+  </script>
+</head>
+<body>
+  <div id="line_top_x"></div>
+</body>
+</html>
 
 
 <!--function drawChart() {
@@ -51,41 +91,3 @@
       ]);*/
 -->
 
-
-
-<html>
-<head>
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['line']});
-      google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Day');
-      data.addColumn('number', 'Guardians of the Galaxy');
-      data.addColumn('number', 'The Avengers');
-      data.addColumn('number', 'Transformers: Age of Extinction');
-
-
-
-
-      data.addRow([
-        [1,  37.8, 80.8, 41.8]
-      ]);
-
-      var options = {
-        title: 'Test_graph'
-      };
-
-      var chart = new google.charts.Line(document.getElementById('line_top_x'));
-
-      chart.draw(data, google.charts.Line.convertOptions(options));
-    }
-  </script>
-</head>
-<body>
-  <div id="line_top_x"></div>
-</body>
-</html>
