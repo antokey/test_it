b2da564fffb2877729ee0b727df3548b5a92651d
JJungJieun <en7615@naver.com>
2018-12-07 05:38:12 -0800

    del_no_effect

--- web/js/js/seaech_box.php
+++
@@ -1,4 +0,0 @@
-<?php
-<input type="hidden" name="foo" value="0" />
-<input type="checkbox" name="foo" value="1">
- ?>
--- web/js/lo/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<script> alert('로그아웃 되었습니다.'); location.replace('http://210.117.181.22/termprj/s201515305/DBproject/index.php'); </script>" ;
-?>
-<html>
-    <meta charset="utf-8"/></head>
-</html>
